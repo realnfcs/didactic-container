d65fc1ad18fd714c90a7519a011b990fee29e314
realnfcs <nicolas.cavalcante.dev@gmail.com>
2023-01-24 17:21:18 -0400

    Configuração inicial do projeto

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "didactic-container",
+	Short: "Repository for learning how containers works in Go.",
+	Long:  `The objective of the project is to create an environment/infrastructure like a Docker to understand concepts and methods such as the creation of containers and their configuration, management and download of images, among others. This repository is learning purpose only, any contribution is welcome.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.didactic-container.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -1,28 +1,11 @@
+/*
+Copyright © 2023 Nicolas Sales <nicolas.cavalcante.dev@gmail.com>
+
+*/
 package main
 
-import (
-	"os"
-
-	"github.com/realnfcs/didactic-container/src/run"
-)
-
-// This is a didactic project with learning propuse
-// Cretids for
-// 			Liz Rice 	@lizrice
-// 			Fabio Akita @akitaonrails
-
-// Project In Progress
-
-// docker 			run image 	<cmd> <params>
-// go run main.go 	run 		<cmd> <params>
+import "github.com/realnfcs/didactic-container/cmd"
 
 func main() {
-	switch os.Args[1] {
-	case "run":
-		run.Run()
-	case "child":
-		run.Child()
-	default:
-		panic("bad command")
-	}
+	cmd.Execute()
 }
